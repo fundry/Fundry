97c7bface6fe57fb1f3327ad2382bc9180b1eada
Nwani Victory <vickywane@gmail.com>
2019-06-04 18:18:10 +0100

    Added CDNS

--- src/components/banner/banner.js
+++ src/components/banner/banner.js
@@ -1,9 +1,15 @@
 import React from "react";
+import styled from "styled-components";
+
+const Div = styled.div`
+  width: 100%
+  background: blue
+`;
 
 export default function home() {
   return (
-    <div>
-      <h1> HOM </h1>
-    </div>
+    <Div>
+      <h1> BANNER HERE </h1>
+    </Div>
   );
 }
