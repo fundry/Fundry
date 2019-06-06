c8e0169e7a68613160919b4dbc4e37466788d3f0
Nwani Victory <vickywane@gmail.com>
2019-06-06 20:44:17 +0100

    Update

--- src/components/about/uses.js
+++ src/components/about/uses.js
@@ -2,13 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { Card } from "react-bootstrap";
 import Flex from "styled-flex-component";
-import { Grid } from "semantic-ui-react";
-
-const Div = styled.div`
-  width: 100%
-  background: #f2f5ff
-  height : 20vh
-`;
+import { Grid, Icon } from "semantic-ui-react";
 
 const Title = styled.p`
 font-weight : bold
@@ -18,33 +12,74 @@ padding-right : 5%
 padding-top : 1%
 `;
 
-const Text = styled.h4`
+const Text = styled.p`
   text-align :  center
   padding-top : 5% 
+  font-size : 1.2em
+`;
+
+const Contain = styled.div`
+  text-align: center;
 `;
 
 export default function home() {
   return (
-    <Card
-      style={{
-        paddingLeft: "2.5%",
-        paddingRight: "2.5%",
-        paddingTop: "2.5%",
-        paddingBottom: "2.5%"
-      }}
-    >
-      <Grid stackable columns="equal">
-        <Grid.Row>
-          <Grid.Column>
-            <Text> Create Application </Text>
-            <Text> Create Application </Text>
-          </Grid.Column>
-          <Grid.Column>
-            <Text> Begin Development </Text>
-            <Text> Begin Development </Text>
-          </Grid.Column>{" "}
-        </Grid.Row>
-      </Grid>
-    </Card>
+    <Flex justifyCenter>
+      <Card
+        style={{
+          paddingLeft: "2.5%",
+          paddingRight: "2.5%",
+          paddingTop: "2.5%",
+          paddingBottom: "2.5%",
+          width: "65%"
+        }}
+      >
+        <Grid stackable columns="equal">
+          <Grid.Row columns="2">
+            <Grid.Column>
+              <Contain>
+                <Icon name="facebook" size="big" />
+                <Text>
+                  {" "}
+                  <strong> Review </strong> your Idea{" "}
+                </Text>
+              </Contain>
+            </Grid.Column>
+            <Grid.Column>
+              <Contain>
+                <Icon name="facebook" size="big" />
+                <Text> Optimize your Idea </Text>
+              </Contain>
+            </Grid.Column>
+            <Grid.Column>
+              <Contain>
+                <Icon name="facebook" size="big" />
+                <Text> Review your Idea </Text>
+              </Contain>
+            </Grid.Column>{" "}
+            <br /> <br /> <br />
+            <br /> <br /> <br />
+            <Grid.Column>
+              <Contain>
+                <Icon name="facebook" size="big" />
+                <Text> Review your Idea </Text>
+              </Contain>
+            </Grid.Column>
+            <Grid.Column>
+              <Contain>
+                <Icon name="facebook" size="big" />
+                <Text> Review your Idea </Text>
+              </Contain>
+            </Grid.Column>
+            <Grid.Column>
+              <Contain>
+                <Icon name="facebook" size="big" />
+                <Text> Review your Idea </Text>
+              </Contain>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Card>
+    </Flex>
   );
 }
