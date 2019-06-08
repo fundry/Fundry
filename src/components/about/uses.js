9118ee6db1ece7fda728e1f4c711de309d8089cf
Nwani Victory <vickywane@gmail.com>
2019-06-08 08:30:20 +0100

    Updates

--- src/components/about/uses.js
+++ src/components/about/uses.js
@@ -3,6 +3,9 @@ import styled from "styled-components";
 import { Card } from "react-bootstrap";
 import Flex from "styled-flex-component";
 import { Grid, Icon } from "semantic-ui-react";
+import { ReactComponent as Rocket } from "./svg/rocket.svg";
+import { ReactComponent as Legal } from "./svg/legal.svg";
+import { ReactComponent as Develop } from "./svg/develop.svg";
 
 const Title = styled.p`
 font-weight : bold
@@ -40,42 +43,49 @@ export default function home() {
           <Grid.Row columns="3">
             <Grid.Column>
               <Contain>
-                <Icon name="facebook" size="big" />
+                <Rocket style={{ maxHeight: "30vh", Bottom: 0 }} />
                 <Text>
-                  {" "}
                   <strong> Review </strong> your Idea{" "}
                 </Text>
               </Contain>
             </Grid.Column>
             <Grid.Column>
               <Contain>
-                <Icon name="facebook" size="big" />
-                <Text> Optimize your Idea </Text>
+                <Rocket style={{ maxHeight: "30vh", Bottom: 0 }} />
+                <Text>
+                  <strong> Optimize </strong> your Idea{" "}
+                </Text>
               </Contain>
             </Grid.Column>
             <Grid.Column>
               <Contain>
-                <Icon name="facebook" size="big" />
-                <Text> Build your Legal Structure </Text>
+                <Legal style={{ maxHeight: "30vh", Bottom: 0 }} />
+                <Text>
+                  {" "}
+                  <strong> Build </strong> your Legal Structure{" "}
+                </Text>
               </Contain>
             </Grid.Column>{" "}
             <br /> <br /> <br />
             <br /> <br /> <br />
             <Grid.Column>
               <Contain>
-                <Icon name="facebook" size="big" />
-                <Text> Develop your Idea </Text>
+                <Develop style={{ maxHeight: "50vh", Bottom: 0 }} />
+                <Text>
+                  {" "}
+                  <strong> Build </strong> the infrastructure{" "}
+                </Text>
               </Contain>
             </Grid.Column>{" "}
             <Grid.Column>
               <Contain>
-                <Icon name="facebook" size="big" />
+                <Rocket style={{ maxHeight: "30vh", Bottom: 0 }} />
                 <Text> Develop your Idea </Text>
               </Contain>
             </Grid.Column>
             <Grid.Column>
               <Contain>
-                <Icon name="facebook" size="big" />
+                <Rocket style={{ maxHeight: "30vh", Bottom: 0 }} />
                 <Text> Raise funds for your service </Text>
               </Contain>
             </Grid.Column>
